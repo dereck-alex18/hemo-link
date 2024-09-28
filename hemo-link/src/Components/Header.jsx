645b10909ad995e211450e14c718e18a00e9abd1
Dereck Portela <dereck_alexander@outlook.com>
2024-09-28 11:37:09 -0300

    add hamburger menu and its animation

--- hemo-link/src/Components/Header.jsx
+++ hemo-link/src/Components/Header.jsx
@@ -1,12 +1,25 @@
-import { Flex, Box, Heading, Button } from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  Heading,
+  Button,
+  IconButton,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 import { Link } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
 
 function Header() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const MotionBox = motion(Box);
+  const MotionIconButton = motion(IconButton);
+
   return (
     <>
       <Flex
         bg="headerColor"
-        direction={["column", "column", "row", "row"]}
+        direction="row"
         gap={["5", "5"]}
         justify="space-between"
         align="center"
@@ -17,7 +30,7 @@ function Header() {
           <a href="#">Hemo Link</a>
         </Heading>
 
-        <Flex gap="3">
+        <Flex gap="3" display={["none", "none", "none", "flex"]}>
           <Link to="/login">
             <Button
               bg="hemoPrimary"
@@ -41,6 +54,69 @@ function Header() {
             Cadastrar
           </Button>
         </Flex>
+
+        <AnimatePresence>
+          <MotionIconButton
+            size="md"
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label="Open Menu"
+            display={{ lg: "none" }}
+            initial={{ rotate: 0, scale: 1 }}
+            animate={isOpen ? { rotate: 180 } : { rotate: 180 }}
+            transition={{ duration: 0.2 }}
+            as={motion.button}
+            zIndex="2"
+            onClick={isOpen ? onClose : onOpen}
+          />
+        </AnimatePresence>
+        <AnimatePresence>
+          {isOpen ? (
+            <MotionBox
+              p={["4", "6"]}
+              h="100vh"
+              w="100vw"
+              position="absolute"
+              top="0"
+              left="0"
+              zIndex="1"
+              bg="headerColor"
+              display={{ lg: "none" }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.5 }}
+            >
+              <Flex
+                direction="column"
+                justify="center"
+                align="center"
+                gap={["4", "12"]}
+              >
+                <Link to="/login">
+                  <Button
+                    bg="headerColor"
+                    color="hemoPrimary"
+                    textDecoration="underline"
+                    fontSize={["2xl", "2xl", "5xl", "5xl"]}
+                    onClick={onClose}
+                  >
+                    Entrar
+                  </Button>
+                </Link>
+                <Button
+                  bg="headerColor"
+                  color="hemoPrimary"
+                  borderRadius="23"
+                  textDecoration="underline"
+                  fontSize={["2xl", "2xl", "5xl", "5xl"]}
+                  onClick={onClose}
+                >
+                  Cadastrar
+                </Button>
+              </Flex>
+            </MotionBox>
+          ) : null}
+        </AnimatePresence>
       </Flex>
     </>
   );
--- hemo-link/src/Components/Login.jsx
+++ hemo-link/src/Components/Login.jsx
@@ -121,6 +121,7 @@ function Login() {
           />
         </Box>
       </Flex>
+
     </>
   );
 }
