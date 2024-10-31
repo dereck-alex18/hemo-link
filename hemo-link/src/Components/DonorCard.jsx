4fc4dd59993cc01dafcb79d18532292cb084f9af
Dereck Portela <dereck_alexander@outlook.com>
2024-10-31 14:33:05 -0300

    add animation to the card

--- hemo-link/src/Components/DonorCard.jsx
+++ hemo-link/src/Components/DonorCard.jsx
@@ -21,6 +21,9 @@ import {
   useBreakpointValue,
 } from "@chakra-ui/react";
 import { PiHeartbeat } from "react-icons/pi";
+import { motion } from "framer-motion";
+
+const MotionBox = motion(Box);
 
 function DonorCard(props) {
   const popoverTrigger = useBreakpointValue({
@@ -29,61 +32,63 @@ function DonorCard(props) {
   });
   return (
     <>
-      <Card bgColor="hemoSecondary" p="5" color="hemoTerciary" maxW="sm">
-        <CardHeader>
-          <Heading size="md">Clinica de Doação de sangue do Recife </Heading>
-          <Popover trigger={popoverTrigger} placement="auto">
-            <PopoverTrigger>
-              <Text mt={3} noOfLines={2} as="b" cursor="pointer">
-                Estamos fazendo doação para repor o estoque de sangue para o
-                carnaval
-              </Text>
-            </PopoverTrigger>
-            <PopoverContent>
-              <PopoverBody>
-                <Text color="textInput">
+      <MotionBox whileHover={{ scale: 1.05 }} transition={{ duration: 0.2 }}>
+        <Card bgColor="hemoSecondary" p="5" color="hemoTerciary" maxW="sm">
+          <CardHeader>
+            <Heading size="md">Clinica de Doação de sangue do Recife </Heading>
+            <Popover trigger={popoverTrigger} placement="top">
+              <PopoverTrigger>
+                <Text mt={3} noOfLines={2} as="b" cursor="pointer">
                   Estamos fazendo doação para repor o estoque de sangue para o
                   carnaval
                 </Text>
-              </PopoverBody>
-            </PopoverContent>
-          </Popover>
-        </CardHeader>
+              </PopoverTrigger>
+              <PopoverContent>
+                <PopoverBody>
+                  <Text color="textInput">
+                    Estamos fazendo doação para repor o estoque de sangue para o
+                    carnaval
+                  </Text>
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
+          </CardHeader>
 
-        <Divider />
+          <Divider />
 
-        <CardBody>
-          <Text mb="1">
-            Data inicial: <Text as="b">31 de Outubro de 2024</Text>
-          </Text>
-          <Text mb="1">
-            Data final: <Text as="b">30 de Novembro de 2024</Text>
-          </Text>
-          <Text mb="1">
-            CEP: <Text as="b">53130410</Text>
-          </Text>
-          <Text mb="1">
-            Cidade: <Text as="b">Recife</Text>
-          </Text>
-          <Text mb="1">
-            Estado: <Text as="b">Pernambuco</Text>
-          </Text>
-          <Text mb="1">
-            Região: <Text as="b">Região metropolitana do Recife</Text>
-          </Text>
-        </CardBody>
+          <CardBody>
+            <Text mb="1">
+              Data inicial: <Text as="b">31 de Outubro de 2024</Text>
+            </Text>
+            <Text mb="1">
+              Data final: <Text as="b">30 de Novembro de 2024</Text>
+            </Text>
+            <Text mb="1">
+              CEP: <Text as="b">53130410</Text>
+            </Text>
+            <Text mb="1">
+              Cidade: <Text as="b">Recife</Text>
+            </Text>
+            <Text mb="1">
+              Estado: <Text as="b">Pernambuco</Text>
+            </Text>
+            <Text mb="1">
+              Região: <Text as="b">Região metropolitana do Recife</Text>
+            </Text>
+          </CardBody>
 
-        <Flex justify="center" alingItems="center" gap={2}>
-          <CardFooter>
-            <Button type="submit" color="textInput">
-              Quero Doar
-              <Box alignSelf="center" ml={1} color="hemoSecondary">
-                <PiHeartbeat />
-              </Box>
-            </Button>
-          </CardFooter>
-        </Flex>
-      </Card>
+          <Flex justify="center" alingItems="center" gap={2}>
+            <CardFooter>
+              <Button type="submit" color="textInput">
+                Quero Doar
+                <Box alignSelf="center" ml={1} color="hemoSecondary">
+                  <PiHeartbeat />
+                </Box>
+              </Button>
+            </CardFooter>
+          </Flex>
+        </Card>
+      </MotionBox>
     </>
   );
 }
