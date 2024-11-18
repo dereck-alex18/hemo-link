77dbeb5c2d6e77cd1f6c381b53cae71f9f45ce79
Dereck Portela <dereck_alexander@outlook.com>
2024-11-18 17:33:50 -0300

    remove log

--- hemo-link/src/api/clinic.js
+++ hemo-link/src/api/clinic.js
@@ -2,7 +2,6 @@ import { baseUrl } from "../helpers/constants";
 
 export const getClinic = async() => {
     try{
-      //  console.log(id);
         const response = await fetch(`${baseUrl}/clinics`);
         return await response.json();
     }catch(e){
