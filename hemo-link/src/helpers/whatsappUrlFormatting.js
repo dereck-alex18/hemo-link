f004f1f02704ad73ba941f37c9ed929b48593d7c
Dereck Portela <dereck_alexander@outlook.com>
2024-11-22 19:39:48 -0300

    check if it's mobile or web for whatsapp triggering

--- hemo-link/src/helpers/whatsappUrlFormatting.js
+++ hemo-link/src/helpers/whatsappUrlFormatting.js
@@ -1,4 +1,5 @@
 export default function whatsUrlFormatting(phoneNumber, donorName, nomeClinica, nomeDaCampanha) {
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
     const message = `
 Olá, ${donorName}. Somos da clinica de doação de sangue "${nomeClinica}".
 Vimos que você se inscreveu na campanha "${nomeDaCampanha}" 
@@ -7,6 +8,9 @@ te enviado esse primeiro contato para confirmarmos o seu
 interesse, caso positivo, agendamos o dia e hora da coleta.
 Vem ser o nosso herói ou heroina! 🦸🦸‍♀️🩸 
 `;
+    if(isMobile){
+        return `whatsapp://send?phone=55${phoneNumber}&text=${encodeURIComponent(message)}`;
+    }
     
     return `https://web.whatsapp.com/send?phone=55${phoneNumber}&text=${encodeURIComponent(message)}`;
 }
